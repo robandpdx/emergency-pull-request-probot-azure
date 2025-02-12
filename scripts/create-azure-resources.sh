06c5dd8a4615d682c6372bbbb156c3ddbe6ee2c9
Rob Anderson <robandpdx@github.com>
2025-02-11 19:31:00 -0600

    update create resources script

--- scripts/create-azure-resources.sh
+++ scripts/create-azure-resources.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
+# Source the .env file
+if [ -f ".env" ]; then
+    source .env
+else
+    echo "Error: .env file not found."
+    exit 1
+fi
+
 # Create a resource group
+echo "Creating resource group $AZURE_RESOURCE_GROUP in $AZURE_LOCATION"
 az group create --name $AZURE_RESOURCE_GROUP --location $AZURE_LOCATION
+sleep 5
 # Create a storage account
-az storage account create --name $AZURE_STORAGE_ACCOUNT --location $AZURE_LOCATION --resource-group AzureFunctionsQuickstart-rg --sku Standard_LRS --allow-blob-public-access false
+echo "Creating storage account $AZURE_STORAGE_ACCOUNT in $AZURE_LOCATION"
+az storage account create --name $AZURE_STORAGE_ACCOUNT --location $AZURE_LOCATION --resource-group $AZURE_RESOURCE_GROUP --sku Standard_LRS --allow-blob-public-access false
+sleep 5
 # Create a function app
+echo "Creating function app $AZURE_FUNCTION_APP in $AZURE_LOCATION"
 az functionapp create --resource-group $AZURE_RESOURCE_GROUP --consumption-plan-location $AZURE_LOCATION --runtime node --runtime-version 20 --functions-version 4 --name $AZURE_FUNCTION_APP --storage-account $AZURE_STORAGE_ACCOUNT
-
+sleep 5
 # Configure the function app settings
-SETTINGS=(
-    APP_ID
-    PRIVATE_KEY
-    WEBHOOK_SECRET
-    SLACK_BOT_TOKEN
-    APPROVE_PR
-    CREATE_ISSUE
-    MERGE_PR
-    SLACK_NOTIFY
-    EMERGENCY_LABEL_PERMANENT
-    ISSUE_TITLE
-    ISSUE_BODY_FILE
-    _ISSUE_ASSIGNEES
-    EMERGENCY_LABEL
-    TRIGGER_STRING
-    SLACKE_CHANNEL_ID
-    SLACK_MESSAGE_FILE
-)
-for setting in "${SETTINGS[@]}"; do
-    az functionapp config appsettings set --name $AZURE_FUNCTION_APP --resource-group $AZURE_RESOURCE_GROUP --settings "$setting"="${!setting}"
-done
-
-# Deploy the function
-func azure functionapp publish $AZURE_FUNCTION_APP
+echo "Configuring function app settings"
+az functionapp config appsettings set --name $AZURE_FUNCTION_APP --resource-group $AZURE_RESOURCE_GROUP --settings \
+    APP_ID="$APP_ID" \
+    PRIVATE_KEY="$PRIVATE_KEY" \
+    WEBHOOK_SECRET="$WEBHOOK_SECRET" \
+    SLACK_BOT_TOKEN="$SLACK_BOT_TOKEN" \
+    APPROVE_PR="$APPROVE_PR" \
+    CREATE_ISSUE="$CREATE_ISSUE" \
+    MERGE_PR="$MERGE_PR" \
+    SLACK_NOTIFY="$SLACK_NOTIFY" \
+    EMERGENCY_LABEL_PERMANENT="$EMERGENCY_LABEL_PERMANENT" \
+    ISSUE_TITLE="$ISSUE_TITLE" \
+    ISSUE_BODY_FILE="$ISSUE_BODY_FILE" \
+    ISSUE_ASSIGNEES="$ISSUE_ASSIGNEES" \
+    EMERGENCY_LABEL="$EMERGENCY_LABEL" \
+    TRIGGER_STRING="$TRIGGER_STRING" \
+    SLACKE_CHANNEL_ID="$SLACKE_CHANNEL_ID" \
+    SLACK_MESSAGE_FILE="$SLACK_MESSAGE_FILE"
